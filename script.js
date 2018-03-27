ceaa39929caffc5e330eac20ab302b55ec58aad0
Gabriel Christino Cordeiro dos Santos <gabriel.christino@gmail.com>
2018-03-27 17:50:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -100,7 +100,7 @@
 						'border-top-right-radius': '0.5em'
 					});
 					$("#headerspam").animate({'font-size': '0em'});
-					varsidebar = 1;
+					varsidebar = 0;
 				} else if ( varsidebar == 0 && desktop == 0 ) {
 					$('#sidebar').slideDown(100);
 					$("#sidebar").animate({width: '80%'});
@@ -112,7 +112,7 @@
 						'font-size': '3.5em',
 						left: '4.5em'
 					});
-					varsidebar = 0;
+					varsidebar = 1;
 				}
 			}
 			function phome() {

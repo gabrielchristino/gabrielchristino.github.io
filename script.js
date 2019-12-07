1e83af1df8ed31dd6011c7358ffe55a5d047811d
Gabriel Christino <gabriel.christino.santos@ibm.com>
2019-12-06 21:18:43 -0300

    css fix

--- script.js
+++ script.js
@@ -6,12 +6,21 @@ function abrirmenu() {
 				menu.style.display = "none";
 		}
 }
-function irPara(caminho) {
-		window.location.href=caminho;
+function irPara(hash) {
+		// window.location.href=hash;
 		menu = document.getElementById("menu");
 		if ( menu.style.display !== "block" ){
 				menu.style.display = "block";
 		} else {
 				menu.style.display = "none";
 		}
+
+		$('html, body').animate({
+			scrollTop: $(hash).offset().top
+		  }, 800, function(){
+	
+			window.location.hash = hash;
+});
 }
+
+

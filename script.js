0da9b50fd243a5d89441e7e10fc4b85a9a0a0edb
gabriel christino cordeiro dos santos <gabriel.christino@gmail.com>
2019-12-10 19:40:56 -0300

    correcoes style

--- script.js
+++ script.js
@@ -9,12 +9,11 @@ function abrirmenu() {
 function irPara(hash) {
 		window.location.href=hash;
 		menu = document.getElementById("menu");
-		if ( menu.style.display !== "block" ){
-				menu.style.display = "block";
-		} else {
-				menu.style.display = "none";
+		if ( window.innerWidth <= 1080 ) {
+			if ( menu.style.display !== "block" ){
+					menu.style.display = "block";
+			} else {
+					menu.style.display = "none";
+			}
 		}
 }
-
-
-

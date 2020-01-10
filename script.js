5ee94e60b7874797b57a2c97be4e570c54f1788f
Gabriel Christino <gabriel.christino.santos@ibm.com>
2020-01-10 08:26:22 -0300

    inclusoes

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 function abrirmenu() {
-		menu = document.getElementById("menu");
-		if ( menu.style.display !== "block" ){
-			$(".menu").css({
-				"opacity":"0",
-				"display":"block",
-			}).show().animate({opacity:1})
-		} else {
-			$(".menu").css({
-				"opacity":"1",
-				"display":"block",
-			}).show().animate({opacity:0})
+	menu = document.getElementById("menu");
+	if (menu.style.display !== "block") {
+		$(".menu").css({
+			"opacity": "0",
+			"display": "block",
+		}).show().animate({ opacity: 1 })
+	} else {
+		$(".menu").css({
+			"opacity": "1",
+			"display": "block",
+		}).show().animate({ opacity: 0 })
 
-			setTimeout(function() {
-	        $(menu).css('display', 'none');
-	    }, 500);
-		}
+		setTimeout(function () {
+			$(menu).css('display', 'none');
+		}, 500);
+	}
 }
 function irPara(hash) {
-		if( window.innerWidth <= 1080 ) {
-			abrirmenu();
-		}
+	if (window.innerWidth <= 1080) {
+		abrirmenu();
+	}
 
-		const position = document.getElementById(hash).offsetTop;
-		$(".corpo").animate({ scrollTop: position }, 1000);
+	const position = document.getElementById(hash).offsetTop;
+	$(".corpo").animate({ scrollTop: position }, 500);
 }
